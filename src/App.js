7b2ce2e6ae86ee199a9f80ba892d9adcecb7c59d
arpesh28 <arpeshgadekar@gmail.com>
2022-11-22 13:05:10 +0530

    calling api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      baseURL: "https://fakestoreapi.com",
+      url: "/products",
+    }).then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Products</h1>
+      <ul>
+        {products.map((prod, index) => (
+          <li>
+            <div>
+              <img src={prod.image} alt="" />
+              <h2>{prod.title}</h2>
+              <h4>${prod.price}</h4>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
