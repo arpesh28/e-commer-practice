f23ff9f9b3d96795de44aa4029fa13751154fa89
arpesh28 <arpeshgadekar@gmail.com>
2022-12-07 12:52:27 +0530

    localstorage

--- src/screens/homescreen.js
+++ src/screens/homescreen.js
@@ -24,11 +24,38 @@ function HomeScreen() {
       // setProducts(response.data);
     });
   }, []);
+  const obj = { name: "arpesh", designation: "dev" };
+  console.log("local:", localStorage.getItem("theme"));
+  const theme = localStorage.getItem("theme");
+  console.log("theme:", theme);
   return (
-    <div className="App">
+    <div className={`App ${theme === "dark" ? "dark" : "light"}`}>
       <h1>
         Products <Link to="/add-product">Create a new product</Link>
       </h1>
+      <button
+        onClick={(e) => {
+          // if (theme === "light" || theme === null) {
+          //   localStorage.setItem("theme", "dark");
+          // } else {
+          //   localStorage.setItem("theme", "light");
+          // }
+          localStorage.setItem("obj", JSON.stringify(obj));
+          // window.location.reload();
+        }}
+      >
+        Toggle Theme
+      </button>{" "}
+      <button
+        onClick={(e) => {
+          // window.location.reload();
+          const obj2 = localStorage.getItem("obj");
+          const convertedObj = JSON.parse(obj2);
+          console.log("obj", convertedObj);
+        }}
+      >
+        Get obj
+      </button>
       <ul className="d-flex">
         {productState.products.map((prod, index) => (
           <li className="product-card">
